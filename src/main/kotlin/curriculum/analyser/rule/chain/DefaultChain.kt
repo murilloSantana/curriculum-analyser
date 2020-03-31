2cb470fcfa31b7726851dba772823aac40988852
murillo.ferreira <murillo.ferreira@bemobi.com.br>
2020-03-30 23:38:59 -0300

    fix chain instantiation

--- src/main/kotlin/curriculum/analyser/rule/chain/DefaultChain.kt
+++ src/main/kotlin/curriculum/analyser/rule/chain/DefaultChain.kt
@@ -2,7 +2,9 @@ package curriculum.analyser.rule.chain
 
 import curriculum.analyser.model.Curriculum
 import curriculum.analyser.rule.validator.PortugueseValidator
+import javax.inject.Singleton
 
+@Singleton
 class DefaultChain: Chain {
     override fun run(curriculum: Curriculum) {
         PortugueseValidator(curriculum, null);
